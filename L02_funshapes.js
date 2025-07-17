4f2efa8154eb7bcf11e80fcc4d1d14da74370a9a
lsorling <lsorling@yahoo.com.sg>
2025-07-17 02:48:46 +0000

    Jul 17, 2025, 2:48 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -15,7 +15,7 @@ function draw() {
   // ellipse(300,200,80,30);
 
   // Challenge 2: A face using circle() function
-  circle(300, 200, 50)
+  circle(300, 200, 200);
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
