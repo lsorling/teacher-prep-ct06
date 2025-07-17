3c24cbadd262800ee8932374af3da3b1e127ded0
lsorling <lsorling@yahoo.com.sg>
2025-07-17 02:57:16 +0000

    Jul 17, 2025, 2:57 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -31,8 +31,9 @@ function draw() {
 
   // Challenge 4: A rocketship using triangle() function
   triangle(200,300, 400,300, 300, 80);
-  // Challenge 5: A square face using quad() function
 
+  // Challenge 5: A square face using quad() function
+  quad(100,100,)
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
