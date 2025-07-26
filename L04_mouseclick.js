ef190aadecb506d12cb13e739e7698ba57e87a9d
lsorling <lsorling@yahoo.com.sg>
2025-07-26 02:52:52 +0000

    Jul 26, 2025, 2:52 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -14,7 +14,7 @@ function preload() {
 // run once like when green flag clicked
 function setup() {
     createCanvas(500, 300);
-    background('#ff3366');
+    background('#cc');
     noStroke();
 
     imageMode(CENTER);
