7162504c959c697b56a8510b53818f9ea52f23b1
lsorling <lsorling@yahoo.com.sg>
2025-07-27 05:00:43 +0000

    Jul 27, 2025, 5:00 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -44,8 +44,8 @@ function draw() {
   for (let r1=0; r1<rows; r1++) {
     posY = (0.5*diameter) + spacing + r1*(diameter+spacing);
     for (let i=0; i<count; i++) {
-      circle(posX, posY, diameter);
       posX = (0.5*diameter) + spacing + i*(diameter+spacing);
+      circle(posX, posY, diameter);
     } 
     text("posY is "+posY, posX, posY);
     // why is posY changed here
