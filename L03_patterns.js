7cd8e19ff06dfd4ade23ebf4a3838908a1fc3a0d
lsorling <lsorling@yahoo.com.sg>
2025-07-27 04:52:36 +0000

    Jul 27, 2025, 4:52 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -4,7 +4,7 @@ function setup() {
     createCanvas(600, 400);
     background(220);
     // note this line here
-    // noLoop(); // Stops continuous drawing
+    noLoop(); // Stops continuous drawing
   }
   
 function draw() {
