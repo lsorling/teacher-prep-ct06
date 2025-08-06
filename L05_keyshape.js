aba270b937e943ecc8eb748f5e1ee52e7ed46763
lsorling <lsorling@yahoo.com.sg>
2025-08-06 13:48:53 +0000

    Aug 6, 2025, 1:48 PM

--- L05_keyshape.js
+++ L05_keyshape.js
@@ -10,10 +10,12 @@ function draw() {
 function keyPressed() {
     if (key === 'c') {
         background(200);
+        fill("red");
         circle(width/2, height/2, 180);
     }
     if (key === 's') {
         background(200);
+        fill("green");
         rect(20, 20, 160, 160);
     }
     if (key === 't') {
