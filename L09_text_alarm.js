6b85de17205f0bfcb1051930ad711b7d007a4960
lsorling <lsorling@yahoo.com.sg>
2025-09-14 03:21:37 +0000

    Sep 14, 2025, 3:21 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -8,6 +8,7 @@ function draw() {
     background(200);
     textSize(72);
     textAlign(CENTER, CENTER);
-    let h = hour()
+    let h = hour();
+    let m = min
     text(nf(hour(),2) + ":" + nf(minute(),2) + ":" + nf(second(),2), width/2, height/2);
 }
