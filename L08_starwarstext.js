d7f9597e223068c2dbb9c485577b6e6f28c17fbc
lsorling <lsorling@yahoo.com.sg>
2025-09-14 03:03:38 +0000

    Sep 14, 2025, 3:03 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -25,14 +25,16 @@ function setup() {
 function draw() {
     background("black");
     //text("hello world!", width/2, ypos);
+
+
+    translate(width/2, ypos);
+    scale(1, 3);
+
     let nextline = 0;
     for (let index=0; index<story.length; index++) {
         text(story[index], 0, ypos+nextline)
         nextline = nextline + 40;
     }
-
-    translate(width/2, ypos);
-    scale(1, 3);
     ypos = ypos - 0.6;
 
     if (ypos < 0) {
