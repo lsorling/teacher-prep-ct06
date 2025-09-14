7c23011757d935f61727bbf363af43310b1f23fa
lsorling <lsorling@yahoo.com.sg>
2025-09-14 03:18:36 +0000

    Sep 14, 2025, 3:18 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,9 +1,11 @@
 // write your codes here
 function setup() {
-
+    createCanvas(600,400);
+    background(200);
 }
 
 function draw() {
+    background(200);
     text(nf(hour(),2), width/2, 50);
     text(nf(minute(),2), width/2, 70);
     text(nf(second(),2), width/2, 90);
