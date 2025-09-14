9e87f0b938374553038fce9d60451caba52bd4a0
lsorling <lsorling@yahoo.com.sg>
2025-09-14 03:09:47 +0000

    Sep 14, 2025, 3:09 AM

--- L09_bouncetext.js
+++ L09_bouncetext.js
@@ -1,6 +1,5 @@
 // write your codes here
 let xpos;
-let story;
 
 function preload() {
 
@@ -14,20 +13,14 @@ function setup() {
     fill("cyan");
     textSize(32);
     textAlign(CENTER, CENTER); // x, y
-
-    story = ["long long time ago,",
-        "there was this little girl.",
-        "her name was Mary...",
-        "she has 3 little lambs."
-    ]
 }
 
 function draw() {
     background("black");
 
     // sequence matters
-    translate(xpos, height/2);
-    scale(1, 3);
+    //translate(xpos, height/2);
+    //scale(1, 3);
 
     // then draw text
     text("I am going to bounce left and right", width/2, height/2);
