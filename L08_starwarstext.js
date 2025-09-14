ced6ffa0800257ea0aa06a38f24f4282bf7ad7b0
lsorling <lsorling@yahoo.com.sg>
2025-09-14 02:47:26 +0000

    Sep 14, 2025, 2:47 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -7,10 +7,12 @@ function preload() {
 
 function setup() {
     createCanvas(600,600);
+    ypos = height;
+
     background("black");
     fill("cyan");
     textSize(32);
-    text("hello world!", 110, 50);
+    text("hello world!", 0, 0);
 }
 
 function draw() {
