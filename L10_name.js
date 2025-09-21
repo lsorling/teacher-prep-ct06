ad0b74a16959b265f43f8826b773a92cbdf6a4f8
lsorling <lsorling@yahoo.com.sg>
2025-09-21 05:03:11 +0000

    Sep 21, 2025, 5:03 AM

--- L10_name.js
+++ L10_name.js
@@ -15,7 +15,8 @@ function preload() {
 function setup() {
     createCanvas(600,600);
 
-    nameInput = createInput()
+    nameInput = createInput();
+    nameInput.input("")
 
     colorPicker = createColorPicker("pink");
     colorPicker.position(285,375);
