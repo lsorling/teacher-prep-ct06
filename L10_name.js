4e76df6b41b15ee5e30cb2219504a02be566b0ad
lsorling <lsorling@yahoo.com.sg>
2025-09-21 05:06:49 +0000

    Sep 21, 2025, 5:06 AM

--- L10_name.js
+++ L10_name.js
@@ -48,6 +48,6 @@ function draw() {
 
     textSize(18);
     textAlign(RIGHT, TOP);
-    text("Enter your name:", 280,300);
+    text("Enter your name:", 280,335);
     text("Pick a background colour:", 280,380);
 }
