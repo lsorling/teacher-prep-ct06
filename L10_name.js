96a9f603b4eec7f6b26c4c4d7449307441f37f97
lsorling <lsorling@yahoo.com.sg>
2025-09-21 05:00:36 +0000

    Sep 21, 2025, 5:00 AM

--- L10_name.js
+++ L10_name.js
@@ -28,7 +28,8 @@ function draw() {
     fill("white");
     rect(70,50,460,250);
 
-    text(yourName, 300,300);
+    yourName = "Charlie";
+    text(yourName, 100,300);
 
     textSize(18);
     textAlign(RIGHT, TOP);
