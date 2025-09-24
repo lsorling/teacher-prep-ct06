7e60c49556392f443420e1ed892387aac7db5675
lsorling <lsorling@yahoo.com.sg>
2025-09-24 05:33:23 +0000

    Sep 24, 2025, 5:33 AM

--- L10_inputform.js
+++ L10_inputform.js
@@ -6,10 +6,13 @@ function setup() {
     // create canvas
     createCanvas(800, 600);
     background(bgColor);
+
+    createSlider()
 }
 
 function draw() {
     // now students can tell me
     // teacher you forget erase
     background(bgColor);
+
 }
