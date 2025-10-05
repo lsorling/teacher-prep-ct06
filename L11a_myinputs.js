17e82a611ead0be10ad7544aa71155a4daf1acc9
lsorling <lsorling@yahoo.com.sg>
2025-10-05 02:56:43 +0000

    Oct 5, 2025, 2:56 AM

--- L11a_myinputs.js
+++ L11a_myinputs.js
@@ -35,5 +35,5 @@ function draw() {
     background(colorPicker.value());
     textAlign(RIGHT,CENTER);
     textSize(18);
-    text("Choose a color:", width/2, height/2+15);
+    text("Choose a color:", width/2-10, height/2+15);
 }
