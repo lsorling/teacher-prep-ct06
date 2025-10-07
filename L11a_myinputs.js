463c7e5643ee7e8ad6228ad7da020a8736b4d1f0
lsorling <lsorling@yahoo.com.sg>
2025-10-07 08:51:44 +0000

    Oct 7, 2025, 8:51 AM

--- L11a_myinputs.js
+++ L11a_myinputs.js
@@ -36,7 +36,7 @@ function setup() {
     colorPicker.position(width/2, 200);
 
     button = createButton("Generate a Story");
-    button.size(50,50);
+    button.size(150,150,50);
     button.position(width/2, 310);
     button.mousePressed(generateStory);
 
