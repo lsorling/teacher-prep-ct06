49bb2c0695e30d56b85c3fba5b142cd604b960ef
lsorling <lsorling@yahoo.com.sg>
2025-10-12 02:52:38 +0000

    Oct 12, 2025, 2:52 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,7 +1,7 @@
 // write your codes here
 let templates = [
     "{noun} {verb} over the wall like humpy dumpy",
-    "{verb} line 2 {noun}",
+    "in Roblox, {verb} l {noun}",
     "{verb} {noun} line 3",
 ];
 
