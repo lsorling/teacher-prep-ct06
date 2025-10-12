798469a28e28c5e54608e0c248374383fb4e4467
lsorling <lsorling@yahoo.com.sg>
2025-10-12 03:03:38 +0000

    Oct 12, 2025, 3:03 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -15,6 +15,7 @@ function draw() {
     fill("cyan");
     rect(50,playerYpos,50,50,10);
     playerYpos = playerYpos+playerVelocityY;
+    playerVelocityY = 
     if (keyIsDown(32)) {
         playerVelocityY = -6;
     }
