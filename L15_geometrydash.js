6a59bb8938a0271a2c72c038c14d366728ca9293
lsorling <lsorling@yahoo.com.sg>
2025-10-12 03:25:44 +0000

    Oct 12, 2025, 3:25 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -24,7 +24,7 @@ function setup() {
     playerVelocityY = 2;
 
     for (let index=0; index<3; index++) {
-        spikesX[index] = index *15 + width-50;
+        spikesX[index] = index *65 + width-50;
     }
 }
 /*
