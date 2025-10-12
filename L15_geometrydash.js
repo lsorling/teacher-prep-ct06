45009f9aa1f6635a20ec4047f6a000ef310c5365
lsorling <lsorling@yahoo.com.sg>
2025-10-12 03:08:38 +0000

    Oct 12, 2025, 3:08 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -18,7 +18,7 @@ let playerVelocityY;
 function setup() {
     createCanvas(800,600);
     background("black");
-    playerYpos = height-60;
+    playerYpos = height-70;
     playerVelocityY = 2
 }
 
@@ -34,7 +34,7 @@ function draw() {
         playerVelocityY = -25;
     }
     // must stop the player from going down further
-    if (playerYpos > height-60) {
-        playerYpos = height-60;
+    if (playerYpos > height-70) {
+        playerYpos = height-70;
     }
 }
